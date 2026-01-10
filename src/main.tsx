04a15753988cdcb67b6aca4785e850c7ee1e21f6
kevinproject08 <diamondking8005@gmail.com>
2026-01-10 08:25:40 -0600

    Remove Vercel analytics injection

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { inject } from "@vercel/analytics/next"
-inject()
 
 createRoot(document.getElementById("root")!).render(<App />);
