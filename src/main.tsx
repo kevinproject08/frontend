82dcce0268f4f50891a84d75c051712cf67e0e67
kevinproject08 <diamondking8005@gmail.com>
2026-01-10 08:32:40 -0600

    Integrate Vercel analytics into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { inject } from "@vercel/analytics/next"
+
+
+inject()
 
 createRoot(document.getElementById("root")!).render(<App />);
